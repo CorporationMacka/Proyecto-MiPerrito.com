a520e08d6e8a8b4f6da6bcb13d700d17b15fe440
CorporatioMacka <yefri.mejia@mackacorporation.com>
2024-12-29 20:14:35 -0500

    Segundo Commit

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+
+
+const  express = require('express');
+
+const cors = require('cors');
+
+const path = require('path');
+
+const paseadoresRoutes = require('./routes/paseadores');
+
+const app = express();
+
+
+
+
