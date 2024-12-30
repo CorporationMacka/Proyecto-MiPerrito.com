7fb31cdeccf06cde128787ce8c16219cbcec9dea
JosephDev <jgiraldo394@outlook.com>
2024-12-30 03:26:15 -0500

    diseño de vistas y validación para login

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+//Función para validar los datos (quemados) del formulario de login
+function validarLogin(event){
+    event.preventDefault(); //Evita que se recargue la página
+    const usuario = document.getElementById('username').value.trim();
+    const contrasena = document.getElementById('password').value.trim();
+
+    // Datos quemados para validar
+    const usuarioCorrecto = "admin";
+    const contrasenaCorrecta = "1234";
+
+    // Validamos que los campos no estén vacíos
+    if(usuario === "" || contrasena === ""){
+        alert('Debes llenar todos los campos');
+        return;
+    }
+
+    // Validamos que los datos sean correctos
+
+    if(usuario === usuarioCorrecto && contrasena === contrasenaCorrecta){
+        // Redirecciona a la vista de paseadores
+        window.location.href = "/views/admin/paseadores/indexPaseadores.html";
+    }else{
+        alert('Usuario o contraseña incorrectos');
+    }
+}
