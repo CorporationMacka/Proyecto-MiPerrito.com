d0f40cb311baf140741310de0f9273d4d324287b
CorporatioMacka <yefri.mejia@mackacorporation.com>
2024-12-29 20:24:33 -0500

    Tercer Commit

--- public/js/app.js
+++ public/js/app.js
@@ -11,5 +11,16 @@ const paseadoresRoutes = require('./routes/paseadores');
 const app = express();
 
 
+ app.use(cors());
 
+ app.use(expres.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api', paseadoresRoutes);
+
+
+module.exports = app;
 
